cb0077face81dd914d542d02cc9baa0d56d2d072
JIHOrrr <131952330+JIHOrrr@users.noreply.github.com>
2024-04-08 15:43:20 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+// firebase config key setup
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB4-rjA5BG-L1l6WA_4pLMYYUrEIac6lOM",
+    authDomain: "login-943f1.firebaseapp.com",
+    projectId: "login-943f1",
+    storageBucket: "login-943f1.appspot.com",
+    messagingSenderId: "377784229097",
+    appId: "1:377784229097:web:6a5dbe5300e8a82fbda478",
+    measurementId: "G-S3DTKPKZCZ"
+}
+
+if (!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
