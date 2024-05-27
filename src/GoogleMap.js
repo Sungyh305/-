5baeab34193e565913ef67b6bd8ba8e4ce1bb735
rhkd1168 <rhkd1168@gmail.com>
2024-05-27 13:00:02 +0900

    Merge branch 'main' of https://github.com/Sungyh305/where-are-you-go
    Polyline

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -17,6 +17,7 @@ const GoogleMap = () => {
     { latitude: 36.798035, longitude: 127.071851 },
     { latitude: 36.798035, longitude: 127.077994 },
     { latitude: 36.797625, longitude: 127.079018 },
+    { latitude: 36.797780, longitude: 127.080352 },
     { latitude: 36.797561, longitude: 127.081822 },
     { latitude: 36.797511, longitude: 127.085860 },
     { latitude: 36.797735, longitude: 127.086208 },
@@ -25,8 +26,40 @@ const GoogleMap = () => {
     { latitude: 36.800411, longitude: 127.086790 },
     { latitude: 36.797952, longitude: 127.087223 },
     { latitude: 36.795607, longitude: 127.087547 },
+    { latitude: 36.799321, longitude: 127.094901 },
+    { latitude: 36.800032, longitude: 127.097574 },
+    { latitude: 36.800400, longitude: 127.099787 },
+    { latitude: 36.794595, longitude: 127.101230 },
+    { latitude: 36.791927, longitude: 127.102007 },
+    { latitude: 36.792334, longitude: 127.102104 },
+    { latitude: 36.792531, longitude: 127.102490 },
+    { latitude: 36.792703, longitude: 127.103576 },
+    { latitude: 36.792864, longitude: 127.103857 },
+    { latitude: 36.793106, longitude: 127.103931 },
+    { latitude: 36.793601, longitude: 127.103839 },
+    { latitude: 36.795012, longitude: 127.103431 },
+    { latitude: 36.795170, longitude: 127.103353 },
+    { latitude: 36.795504, longitude: 127.102992 },
+    { latitude: 36.795546, longitude: 127.102712 },
+    { latitude: 36.795300, longitude: 127.101055 },
     // 좌표 추가
   ];
+
+  const PolylineCoordinates02 = [
+    { latitude: 36.799455, longitude: 127.073997 },
+    { latitude: 36.799510, longitude: 127.077940 },
+    { latitude: 36.798035, longitude: 127.077994 },
+    { latitude: 36.797625, longitude: 127.079018 },
+    { latitude: 36.797780, longitude: 127.080352 },
+    { latitude: 36.797561, longitude: 127.081822 },
+    { latitude: 36.797511, longitude: 127.085860 },
+    { latitude: 36.797210, longitude: 127.086117 },
+    { latitude: 36.796507, longitude: 127.085858 },
+    { latitude: 36.795391, longitude: 127.085773 },
+    { latitude: 36.794987, longitude: 127.085567 },
+    { latitude: 36.794721, longitude: 127.085651 },
+    { latitude: 36.795607, longitude: 127.087547 },
+  ]
 
   useEffect(() => {
     let socket;
@@ -38,7 +71,7 @@ const GoogleMap = () => {
       socket.on('broadcastLocation', (location) => {
         // 범위 id 구별하여 이미지 분리
         if (location.key.startsWith('test')) {newMarkerImage = require('../assets/Person_expo.png');}
-        else {newMarkerImage = require('../assets/bus_expo.png');}
+        else {newMarkerImage = require('../assets/bus_icon.png');}
 
         setUserMarkers((prevMarkers) => ({
           ...prevMarkers,
@@ -93,7 +126,12 @@ const GoogleMap = () => {
         <Polyline
           coordinates={PolylineCoordinates}
           strokeWidth={4}
-          strokeColor="rgba(255, 165, 0, 0.5)"
+          strokeColor="rgba(255, 165, 0, 1)"
+        />
+        <Polyline
+          coordinates={PolylineCoordinates02}
+          strokeWidth={4}
+          strokeColor="rgba(255, 165, 0, 1)"
         />
 
         {/* 사용자 마커 표시 */}
--- src/dataset.js
+++ src/dataset.js
@@ -75,7 +75,469 @@ const ranges = [
       { latitude: 36.797428, longitude: 127.081587 },
       { latitude: 36.797650, longitude: 127.085760 }
     ]
-  }
+  },
+  {
+    id: "range9",
+    points: [
+      { latitude: 36.797805, longitude: 127.085800 },
+      { latitude: 36.797185, longitude: 127.086250 }
+    ]
+  },
+  {
+    id: "range10",
+    points: [
+      { latitude: 37.797808, longitude: 127.086280 },
+      { latitude: 36.799798, longitude: 127.085875 }
+    ]
+  },
+  {
+    id: "range11",
+    points: [
+      { latitude: 36.799844, longitude: 127.085991 },
+      { latitude: 36.800246, longitude: 127.085618 }
+    ]
+  },
+  {
+    id: "range12",
+    points: [
+      { latitude: 36.800246, longitude: 127.085618 },
+      { latitude: 36.800477, longitude: 127.086715 }
+    ]
+  },
+  {
+    id: "range13",
+    points: [
+      { latitude: 36.800477, longitude: 127.086715 },
+      { latitude: 36.797924, longitude: 127.087310 }
+    ]
+  },
+  {
+    id: "range14",
+    points: [
+      { latitude: 36.797920, longitude: 127.087028 },
+      { latitude: 36.797000, longitude: 127.087574}
+    ]
+  },
+  {
+    id: "range15",
+    points: [
+      { latitude: 36.796976, longitude: 127.087537 },
+      { latitude: 36.795857, longitude: 127.087780}
+    ]
+  },
+  {
+    id: "range16",
+    points: [
+      { latitude: 36.795860, longitude: 127.087786 },
+      { latitude: 36.795307, longitude: 127.087174}
+    ]
+  },
+  {
+    id: "range17",
+    points: [
+      { latitude: 36.797190, longitude: 127.086169 },
+      { latitude: 36.796397, longitude: 127.085773}
+    ]
+  },
+  {
+    id: "range18",
+    points: [
+      { latitude: 36.796400, longitude: 127.085915},
+      { latitude: 36.795444, longitude: 127.085711}
+    ]
+  },
+  {
+    id: "range19",
+    points: [
+      { latitude: 36.795427, longitude: 127.085420},
+      { latitude: 36.794314, longitude: 127.086050}
+    ]
+  },
+  {
+    id: "range20",
+    points: [
+      { latitude: 36.794834, longitude: 127.086004},
+      { latitude: 36.795311, longitude: 127.086630}
+    ]
+  },
+  {
+    id: "range21",
+    points: [
+      { latitude: 36.795158, longitude: 127.086661},
+      { latitude: 36.795698, longitude: 127.087167}
+    ]
+  },
+  {
+    id: "range22",
+    points: [
+      { latitude: 36.795688, longitude: 127.087811},
+      { latitude: 36.796356, longitude: 127.088446}
+    ]
+  },
+  {
+    id: "range23",
+    points: [
+      { latitude: 36.796022, longitude: 127.088526},
+      { latitude: 36.796829, longitude: 127.089348}
+    ]
+  },
+  {
+    id: "range24",
+    points: [
+      { latitude: 36.796424, longitude: 127.089365},
+      { latitude: 36.796893, longitude: 127.089463}
+    ]
+  },
+  {
+    id: "range25",
+    points: [
+      { latitude: 36.796633, longitude: 127.089730},
+      { latitude: 36.797213, longitude: 127.090089}
+    ]
+  },
+  {
+    id: "range26",
+    points: [
+      { latitude: 36.797334, longitude: 127.091084},
+      { latitude: 36.797874, longitude: 127.091364}
+    ]
+  },
+  {
+    id: "range27",
+    points: [
+      { latitude: 36.797522, longitude: 127.091439},
+      { latitude: 36.797629, longitude: 127.090893}
+    ]
+  },
+  {
+    id: "range28",
+    points: [
+      { latitude: 36.797270, longitude: 127.090955},
+      { latitude: 36.797828, longitude: 127.091278}
+    ]
+  },
+  {
+    id: "range29",
+    points: [
+      { latitude: 36.797490, longitude: 127.091362},
+      { latitude: 36.798084, longitude: 127.091766}
+    ]
+  },
+  {
+    id: "range30",
+    points: [
+      { latitude: 36.797743, longitude: 127.091846},
+      { latitude: 36.798266, longitude: 127.092144}
+    ]
+  },
+  {
+    id: "range31",
+    points: [
+      { latitude: 36.797931, longitude: 127.092197},
+      { latitude: 36.798507, longitude: 127.092619}
+    ]
+  },
+  {
+    id: "range32",
+    points: [
+      { latitude: 36.798127, longitude: 127.092628},
+      { latitude: 36.798646, longitude: 127.092944}
+    ]
+  },
+  {
+    id: "range33",
+    points: [
+      { latitude: 36.798283, longitude: 127.092935},
+      { latitude: 36.798841, longitude: 127.093334}
+    ]
+  },
+  {
+    id: "range34",
+    points: [
+      { latitude: 36.798521, longitude: 127.093392},
+      { latitude: 36.799038, longitude: 127.093772}
+    ]
+  },
+  {
+    id: "range35",
+    points: [
+      { latitude: 36.798714, longitude: 127.093782},
+      { latitude: 36.799188, longitude: 127.094175}
+    ]
+  },
+  {
+    id: "range36",
+    points: [
+      { latitude: 36.798867, longitude: 127.094206},
+      { latitude: 36.799483, longitude: 127.094924}
+    ]
+  },
+  {
+    id: "range37",
+    points: [
+      { latitude: 36.799177, longitude: 127.094979},
+      { latitude: 36.799613, longitude: 127.095369}
+    ]
+  },
+  {
+    id: "range38",
+    points: [
+      { latitude: 36.799312, longitude: 127.095411},
+      { latitude: 36.799677, longitude: 127.095576}
+    ]
+  },
+  {
+    id: "range39",
+    points: [
+      { latitude: 36.799373, longitude: 127.095628},
+      { latitude: 36.799818, longitude: 127.096115}
+    ]
+  },
+  {
+    id: "range40",
+    points: [
+      { latitude: 36.799500, longitude: 127.096149},
+      { latitude: 36.799975, longitude: 127.096667}
+    ]
+  },
+  {
+    id: "range41",
+    points: [
+      { latitude: 36.799649, longitude: 127.096719},
+      { latitude: 36.800086, longitude: 127.097077}
+    ]
+  },
+  {
+    id: "range42",
+    points: [
+      { latitude: 36.799754, longitude: 127.097112},
+      { latitude: 36.800213, longitude: 127.097554}
+    ]
+  },
+  {
+    id: "range43",
+    points: [
+      { latitude: 36.799901, longitude: 127.097625},
+      { latitude: 36.800282, longitude: 127.097984}
+    ]
+  },
+  {
+    id: "range44",
+    points: [
+      { latitude: 36.799972, longitude: 127.098008},
+      { latitude: 36.800351, longitude: 127.098401}
+    ]
+  },
+  {
+    id: "range45",
+    points: [
+      { latitude: 36.800030, longitude: 127.098435},
+      { latitude: 36.800434, longitude: 127.098898}
+    ]
+  },
+  {
+    id: "range46",
+    points: [
+      { latitude: 36.800099, longitude: 127.098922},
+      { latitude: 36.800525, longitude: 127.099494}
+    ]
+  },
+  {
+    id: "range47",
+    points: [
+      { latitude: 36.800525 ,longitude: 127.099494},
+      { latitude: 36.799970, longitude: 127.100153}
+    ]
+  },
+  {
+    id: "range48",
+    points: [
+      { latitude: 36.799953 ,longitude: 127.099618},
+      { latitude: 36.799547, longitude: 127.100243}
+    ]
+  },
+  {
+    id: "range49",
+    points: [
+      { latitude: 36.799539 ,longitude: 127.099739},
+      { latitude: 36.798917, longitude: 127.100394}
+    ]
+  },
+  {
+    id: "range50",
+    points: [
+      { latitude: 36.798908 ,longitude: 127.099902},
+      { latitude: 36.798051, longitude: 127.100588}
+    ]
+  },
+  {
+    id: "range51",
+    points: [
+      { latitude: 36.798054 ,longitude: 127.100225},
+      { latitude: 36.797449, longitude: 127.100753}
+    ]
+  },
+  {
+    id: "range52",
+    points: [
+      { latitude: 36.797414 ,longitude: 127.100362},
+      { latitude: 36.799783, longitude: 127.100923}
+    ]
+  },
+  {
+    id: "range53",
+    points: [
+      { latitude: 36.796780 ,longitude: 127.100519},
+      { latitude: 36.796117, longitude: 127.101085}
+    ]
+  },
+  {
+    id: "range54",
+    points: [
+      { latitude: 36.796108 ,longitude: 127.100681},
+      { latitude: 36.795396, longitude: 127.101270}
+    ]
+  },
+  {
+    id: "range55",
+    points: [
+      { latitude: 36.795370 ,longitude: 127.100838},
+      { latitude: 36.795134, longitude: 127.101327}
+    ]
+  },
+  {
+    id: "range56",
+    points: [
+      { latitude: 36.795154,longitude: 127.100873},
+      { latitude: 36.794527,longitude: 127.101490}
+    ]
+  },
+  {
+    id: "range57",
+    points: [
+      { latitude: 36.794501,longitude: 127.101029},
+      { latitude: 36.793895,longitude: 127.101667}
+    ]
+  },
+  {
+    id: "range58",
+    points: [
+      { latitude: 36.793861,longitude: 127.101200},
+      { latitude: 36.793240,longitude: 127.101833}
+    ]
+  },
+  {
+    id: "range59",
+    points: [
+      { latitude: 36.793189,longitude: 127.101388},
+      { latitude: 36.792596,longitude: 127.102021}
+    ]
+  },
+  {
+    id: "range60",
+    points: [
+      { latitude: 36.792536,longitude: 127.101581},
+      { latitude: 36.791861,longitude: 127.102236}
+    ]
+  },
+  {
+    id: "range61",
+    points: [
+      { latitude: 36.792351,longitude: 127.102236},
+      { latitude: 36.792583,longitude: 127.102514}
+    ]
+  },
+  {
+    id: "range62",
+    points: [
+      { latitude: 36.792437,longitude: 127.102547},
+      { latitude: 36.792682,longitude: 127.103029}
+    ]
+  },
+  {
+    id: "range63",
+    points: [
+      { latitude: 36.792531,longitude: 127.103029},
+      { latitude: 36.792780,longitude: 127.103491}
+    ]
+  },
+  {
+    id: "range64",
+    points: [
+      { latitude: 36.792613,longitude: 127.103523},
+      { latitude: 36.792866,longitude: 127.103888}
+    ]
+  },
+  {
+    id: "range65",
+    points: [
+      { latitude: 36.792879,longitude: 127.103609},
+      { latitude: 36.792952,longitude: 127.103941}
+    ]
+  },
+  {
+    id: "range66",
+    points: [
+      { latitude: 36.792952,longitude: 127.103941},
+      { latitude: 36.793296,longitude: 127.103614}
+    ]
+  },
+  {
+    id: "bus_stop2",
+    points: [
+      { latitude: 36.793313,longitude: 127.103984},
+      { latitude: 36.793605,longitude: 127.103518}
+    ]
+  },
+  {
+    id: "bus_stop3",
+    points: [
+      { latitude: 36.793652,longitude: 127.103893},
+      { latitude: 36.794297,longitude: 127.103319}
+    ]
+  },
+  {
+    id: "bus_stop4",
+    points: [
+      { latitude: 36.794357,longitude: 127.103689},
+      { latitude: 36.794593,longitude: 127.103244}
+    ]
+  },
+  {
+    id: "bus_stop5",
+    points: [
+      { latitude: 36.794632,longitude: 127.103609},
+      { latitude: 36.795126,longitude: 127.103099}
+    ]
+  },
+  {
+    id: "range 67",
+    points: [
+      { latitude: 36.795139,longitude: 127.103442},
+      { latitude: 36.795306,longitude: 127.103036}
+    ]
+  },
+  {
+    id: "range 68",
+    points: [
+      { latitude: 36.795319,longitude: 127.103331},
+      { latitude: 36.795590,longitude: 127.102950}
+    ]
+  },
+  {
+    id: "range 69",
+    points: [
+      { latitude: 36.795590,longitude: 127.102950},
+      { latitude: 36.795371,longitude: 127.101931}
+    ]
+  },
+  {
+    id: "range 70",
+    points: [
+      { latitude: 36.795499,longitude: 127.101909},
+      { latitude: 36.795259,longitude: 127.101319}
+    ]
+  },
 ];
 
 module.exports = ranges;
