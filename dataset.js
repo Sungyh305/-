274d4e67b96075248ce8157f129d62d306ea3f9a
sangchan25 <162412930+sangchan25@users.noreply.github.com>
2024-06-05 17:17:39 +0900

    Add files via upload

---
+++ dataset.js
@@ -0,0 +1,41 @@
+const ranges = [
+
+  {
+    id: "range100",
+    points: [
+      { latitude: 36.800727 ,longitude: 127.093444},
+      { latitude: 36.798111, longitude: 127.099694}
+    ]
+  },
+  {
+    id: "range101",
+    points: [
+      { latitude: 36.799471 ,longitude: 127.098915},
+      { latitude: 36.802772, longitude: 127.106628}
+    ]
+  },
+  {
+    id: "range102",
+    points: [
+      { latitude: 36.800027 ,longitude: 127.104181},
+      { latitude: 36.803859, longitude: 127.111796}
+    ]
+  },
+  {
+    id: "range103",
+    points: [
+      { latitude: 36.802798 ,longitude: 127.114131},
+      { latitude: 36.799327, longitude: 127.117365}
+    ]
+  },
+  {
+    id: "range104",
+    points: [
+      { latitude: 36.822459 ,longitude: 127.152380},
+      { latitude: 36.823176, longitude: 127.152677}
+    ]
+  },
+
+  ];
+
+module.exports = ranges;
